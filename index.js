2077897f4135325cd06aa0ed27ca93ee5cf3e38c
Gr8Tortuga <jasonkiser@gmail.com>
2017-10-04 02:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return(`It\'s ${day} days until ${holiday}!`)
+  return(`It\'s ${days} days until ${holiday}!`)
 }
